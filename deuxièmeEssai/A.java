01cfddaf140c697a8b51f05e99867e45712a8d46
mohamadfawaz <mohamad.fawaz@isae.edu.lb>
2022-11-27 16:53:55 +0200

    init commit

--- deuxièmeEssai/A.java
+++ deuxièmeEssai/A.java
@@ -2,8 +2,8 @@
 /**
  * Décrivez votre classe A ici.
  *
- * @author (votre nom)
- * @version (un numéro de version ou une date)
+ * @author Mohamad fawaz
+ * @version 27/11/2022
  */
 public class A
 {
